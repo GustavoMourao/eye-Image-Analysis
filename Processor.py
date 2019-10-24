7049c728b96bab79481867fe9e78fc548d837622
Gustavo <gustavolmourao@gmail.com>
2019-10-23 23:36:44 -0300

    fix issue: unit test and processor class

--- Processor.py
+++ Processor.py
@@ -3,84 +3,51 @@ from scipy import fftpack
 import numpy as np
 from scipy import ndimage
 import cv2
+from skimage.metrics import peak_signal_noise_ratio
 
 
 class Processor:
     """
+    Class that implements classical signal processing image methods, as
+    noise estimation and signal-to-noise ratio based on clean image.
     """
-    def __init__(self, image):
+    def __init__(self, noisy_image):
         """
         Get raw image on cv2 processing format.
+
+        Args:
+        ---------
+            noisy_image: noisy image
         """
-        self.image = image
-        self.d_theta = 30
-        self.r_min = 20
-        self.r_max = 250
+        self.noisy_image = noisy_image
 
     def noise_estimation(self):
         """
         Estimates the power spectrum of noise image.
+        Based on Robust wavelet-based estimator of the (Gaussian) noise standard deviation.
+        (Wrapper of skimage.restoration)
+
+        Return:
+        ---------
+            estimated_sigma: estimated noise standard deviation(s)        
         """
         return estimate_sigma(
-            self.image,
+            self.noisy_image,
             multichannel=True,
             average_sigmas=True
         )
 
-    def get_global_snr(self):
+    def get_global_snr(self, clean_image):
         """
-        Get signal-to-noise ratio of the image in dB.
+        Compute the peak signal to noise ratio (PSNR) for an image.
+        (Wrapper of skimage.metrics)
+
+        Args:
+        ---------
+            clean_image: reference image
+
+        Return:
+        ---------
+            psnr: peak signal to noise ratio
         """
-        noise_power = self.noise_estimation()
-        fft_image = fftpack.fft2(self.image)
-        fft_image_shift = fftpack.fftshift(fft_image)
-        psd2D = np.abs(fft_image_shift)
-        # TODO: Find a way to estimate the psd or to establishment a ideal value of \sigma!
-
-        angF, psd1DD = self.get_rpsd(
-            psd2D,
-            self.d_theta,
-            self.r_min,
-            self.r_min
-        )
-
-        psd1DD = np.mean(psd1DD)
-        mu_signal = np.mean(psd1DD)
-        snr = 20*np.log10(mu_signal/noise_power)
-
-        return snr
-
-    def get_rpsd(self, psd2D, dTheta, rMin, rMax):
-        """
-        Reference: https://gist.github.com/TangibitStudios
-        TODO: Evaluates if there are other methods
-        """
-        h  = psd2D.shape[0]
-        w  = psd2D.shape[1]
-        wc = w//2
-        hc = h//2
-    
-        # note that displaying PSD as image inverts Y axis
-        # create an array of integer angular slices of dTheta
-        Y, X  = np.ogrid[0:h, 0:w]
-        theta = np.rad2deg(np.arctan2(-(Y-hc), (X-wc)))
-        theta = np.mod(theta + dTheta/2 + 360, 360)
-        theta = dTheta * (theta//dTheta)
-        theta = theta.astype(np.int)
-    
-        # mask below rMin and above rMax by setting to -100
-        R     = np.hypot(-(Y-hc), (X-wc))
-        mask  = np.logical_and(R > rMin, R < rMax)
-        theta = theta + 100
-        theta = np.multiply(mask, theta)
-        theta = theta - 100
-    
-        # SUM all psd2D pixels with label 'theta' for 0<=theta❤60 between rMin and rMax
-        angF  = np.arange(0, 360, int(dTheta))
-        psd1D = ndimage.sum(psd2D, theta, index=angF)
-    
-        # normalize each sector to the total sector power
-        pwrTotal = np.sum(psd1D)
-        psd1D    = psd1D/pwrTotal
-    
-        return angF, psd1D
+        return peak_signal_noise_ratio(clean_image, self.noisy_image)
