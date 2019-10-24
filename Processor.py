1bef6faf6f6f79121aa86cb99d1a41d81f2916be
Gustavo <gustavolmourao@gmail.com>
2019-10-24 18:07:51 -0300

    histogram and filter unit test

--- Processor.py
+++ Processor.py
@@ -2,6 +2,8 @@ from skimage.restoration import estimate_sigma
 import cv2
 from skimage.metrics import peak_signal_noise_ratio
 from skimage import filters
+from skimage import restoration
+import numpy as np
 
 
 class Processor:
@@ -15,29 +17,11 @@ class Processor:
 
         Args:
         ---------
-            noisy_image: noisy image
+            noisy_image: noisy image (without processing steps)
         """
         self.noisy_image = noisy_image
 
-    def noise_estimation(self):
-        """
-        Estimates the power spectrum of noise image.
-        Based on Robust wavelet-based estimator of the (Gaussian) noise standard deviation.
-
-        Obs:
-            Wrapper of skimage.restoration
-
-        Return:
-        ---------
-            estimated_sigma: estimated noise standard deviation(s)
-        """
-        return estimate_sigma(
-            self.noisy_image,
-            multichannel=True,
-            average_sigmas=True
-        )
-
-    def get_global_snr(self, filtered_image):
+    def get_global_psnr(self, filtered_image):
         """
         Compute the peak signal to noise ratio (PSNR) for an image.
 
@@ -52,9 +36,34 @@ class Processor:
         ---------
             psnr: peak signal to noise ratio
         """
-        return peak_signal_noise_ratio(filtered_image, self.noisy_image)
+        return peak_signal_noise_ratio(
+            filtered_image,
+            self.noisy_image
+        )
 
-    def filter_mean(self):
+    def noise_estimation(self, image):
+        """
+        Estimates the power spectrum of noise image.
+        Based on Robust wavelet-based estimator of the (Gaussian)
+        noise standard deviation.
+
+        Obs:
+            Wrapper of skimage.restoration
+
+        Args:
+        ---------
+            image: image to apply processing step
+        Return:
+        ---------
+            estimated_sigma: estimated noise standard deviation
+        """
+        return estimate_sigma(
+            image,
+            multichannel=True,
+            average_sigmas=True
+        )
+
+    def filter_mean(self, image):
         """
         Filter image based on mean mask (linear). Convolves image with
         normalized box filter.
@@ -62,13 +71,21 @@ class Processor:
         Obs:
             Wrapper of opencv
 
+        Args:
+        ---------
+            image: image to apply processing step
+
         Return:
         ---------
             image_denoised: denoised image
         """
-        return cv2.boxFilter(self.noisy_image, -1, (3,3))
+        return cv2.boxFilter(
+            image,
+            -1,
+            (3, 3)
+        )
 
-    def filter_median(self):
+    def filter_median(self, image):
         """
         Filter image based on median mask (non-linear). Each output is computed
         as the median value of the oinút samples under the analyzed window.
@@ -76,21 +93,81 @@ class Processor:
         Obs:
             Wrapper of skimage
 
+        Args:
+        ---------
+            image: image to apply processing step
+
         Return:
         ---------
             image_denoised: denoised image
         """
-        return filters.median(self.noisy_image)
+        return filters.median(image)
 
-    def filter_wiener(self):
+    def filter_wiener(self, image):
         """
         Filter image based on Wiener mask (non-linear).
 
         Obs:
             Wrapper of skimage
 
+        Args:
+        ---------
+            image: image to apply processing step
+
         Return:
         ---------
             image_denoised: denoised image
         """
-        return filters.wiener(self.noisy_image, None)
+        return restoration.wiener(
+            image,
+            np.ones((5, 5)) / 25,
+            1100
+        )
+
+    def get_histogram(self, image):
+        """
+        Get histogram of image (tipically before filter signal).
+
+        Obs:
+            Wrapper of cv2
+
+        Args:
+        ---------
+            image: image to apply processing step
+
+        Return:
+        ---------
+            image_hist: image histogram
+        """
+        return cv2.calcHist(
+            image,
+            [0],
+            None,
+            [256],
+            [10, 256]
+        )
+
+    def get_histogram_equalized(self, image):
+        """
+        Histogram equalization of image (tipically before filter signal).
+
+        Obs:
+            Wrapper of cv2
+
+        Args:
+        ---------
+            image: image to apply processing step
+
+        Return:
+        ---------
+            image_equalized: histogram image equalized
+        """
+        # img_eql = cv2.equalizeHist(image)
+        return cv2.calcHist(
+            # img_eql,
+            cv2.equalizeHist(image),
+            [0],
+            None,
+            [256],
+            [10, 256]
+        )
--- ProcessorTest.py
+++ ProcessorTest.py
@@ -2,75 +2,83 @@ import unittest
 import cv2
 import os
 from Processor import Processor
-import numpy as np
-import matplotlib.pyplot as plt
 from skimage.util import random_noise
+import numpy as np
 
 
 class ProcessorTest(unittest.TestCase):
     """
     Class responsible to call unit test methods of Processor class.
     """
-    #def test_get_global_snr(self):
-    #    """
-    #    Unit test that verify if image, after eliminates noise,
-    #    has SNR above the defined threshold. Higher values of SNR
-    #    indicates that the 
-    #    """
-    #    # Arrange
-    #    # TODO: Verify for each image or for randomly images?
-    #    # 1. Get reference image (base line image)
-    #    # 2. Get raw image
-    #    # 3. Denoise image
-    #    image = cv2.imread('02_h.jpg', 0)
-    #
-    #    # Act
-    #    processor = Processor(image)
-    #    global_snr = processor.get_global_snr(image)
-    #
-    #    # Assert
-    #    assert global_snr > -20
-    #
-    #def test_noise_estimation(self):
-    #    """
-    #    Unit test that verify if noise estimation of filtered image
-    #    is above defined threshold.
-    #    """
-    #    # Arrange
-    #    # os.chdir('healthy')
-    #    image = cv2.imread('02_h.jpg', 0)
-    #
-    #    # Act
-    #    processor = Processor(image)
-    #    sigma_noise = processor.noise_estimation()
-    #
-    #    # Assert
-    #    assert sigma_noise > 0.1
-
     def test_filter(self):
         """
         Unit test that verify if noise estimation of filtered image
         is above defined threshold.
         """
-        # Arrange
+        # Arrange: Get figure and add noise (salt and pepper).
         image_ref = cv2.imread('02_h.jpg', 0)
         noise_image = random_noise(image_ref, mode='s&p', amount=0.3)
 
-        # Act
+        # Act.
         processor = Processor(noise_image)
-        filtered_img_mean = processor.filter_mean()       
-        filtered_img_mdan = processor.filter_median()
-        # BUG: It is broken over here!
-        filtered_img_wner = processor.filter_wiener()
+        filtered_img_mean = processor.filter_mean(noise_image)
+        filtered_img_mdan = processor.filter_median(noise_image)
+        filtered_img_wner = processor.filter_wiener(noise_image)
 
-        snr_filter_mean = processor.get_global_snr(filtered_img_mean)
-        snr_filter_mdan = processor.get_global_snr(filtered_img_mdan)
-        snr_filter_wner = processor.get_global_snr(filtered_img_wner)
+        psnr_filter_mean = processor.get_global_psnr(filtered_img_mean)
+        psnr_filter_mdan = processor.get_global_psnr(filtered_img_mdan)
+        psnr_filter_wner = processor.get_global_psnr(filtered_img_wner)
 
-        # Assert
-        assert snr_filter_mean > 0
-        assert snr_filter_mdan > 0
-        assert snr_filter_wner > 0
+        print('SNR after Mean filter: ', psnr_filter_mean)
+        print('SNR after Median filter: ', psnr_filter_mdan)
+        print('SNR after Wiener filter: ', psnr_filter_wner)
+
+        # Assert.
+        assert psnr_filter_mean > 0
+        assert psnr_filter_mdan > 0
+        assert psnr_filter_wner > 0
+
+    def test_histogram_equalizer(self):
+        """
+        Unit test that verify if noise estimation of filtered image
+        is above defined threshold.
+        """
+        # Arrange.
+        image_ref = cv2.imread('02_h.jpg', 0)
+
+        # Act: Get histogram of image without equalization.
+        processor = Processor(image_ref)
+        hist_imag_ref = processor.get_histogram(image_ref)
+        mean_hist_imag_ref = np.mean(
+            hist_imag_ref / np.max(hist_imag_ref)
+        )
+        std_hist_imag_ref = np.std(
+            hist_imag_ref / np.max(hist_imag_ref)
+        )
+        hist_imag_ref_equl = processor.get_histogram_equalized(image_ref)
+        mean_hist_imag_ref_equl = np.mean(
+            hist_imag_ref_equl / np.max(hist_imag_ref_equl)
+        )
+        std_hist_imag_ref_equl = np.std(
+            hist_imag_ref_equl / np.max(hist_imag_ref_equl)
+        )
+
+        print(
+            'Histogram statistics of raw image: [mean] = ',
+            mean_hist_imag_ref,
+            '[STD] = ',
+            std_hist_imag_ref
+        )
+        print(
+            'Histogram statistics of equalized image: [mean] = ',
+            mean_hist_imag_ref_equl,
+            '[STD] = ',
+            std_hist_imag_ref_equl
+        )
+
+        # Assert: evaluate variance since is a measure of image constrast
+        assert mean_hist_imag_ref_equl < mean_hist_imag_ref
+        assert std_hist_imag_ref_equl < std_hist_imag_ref
 
 
 if __name__ == '__main__':
@@ -78,6 +86,4 @@ if __name__ == '__main__':
     Call unit test methods.
     """
     os.chdir('healthy')
-    proc = ProcessorTest()
-    proc.test_filter()
     unittest.main()
