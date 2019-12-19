66aa768b939559c02282ceb55f9eece15f7dc026
Gustavo <gustavolmourao@gmail.com>
2019-12-19 16:03:05 -0300

    add resnetv1 model

--- Interpreter.py
+++ Interpreter.py
@@ -12,23 +12,48 @@ from keras.models import Model
 # from WindowOpt.WindowsOpt import *
 from Graphs import Graphs
 # import efficientnet.keras as efn
-from keras import backend as K
 # import tensorflow as tf
 from keras.layers.normalization import BatchNormalization
 from sklearn.metrics import accuracy_score
-from efficientnet import EfficientNetB0 as Net0
-from efficientnet import EfficientNetB1 as Net1
-from efficientnet import EfficientNetB2 as Net2
-from efficientnet import EfficientNetB3 as Net3
-from efficientnet import EfficientNetB4 as Net4
-from efficientnet import EfficientNetB5 as Net5
-from efficientnet import EfficientNetB6 as Net6
-from efficientnet import EfficientNetB7 as Net7
-from efficientnet import center_crop_and_resize, preprocess_input
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB0 as Net0
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB1 as Net1
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB2 as Net2
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB3 as Net3
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB4 as Net4
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB5 as Net5
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB6 as Net6
+from efficientnet_keras_transfer_learning.efficientnet import EfficientNetB7 as Net7
+from efficientnet_keras_transfer_learning.efficientnet import center_crop_and_resize, preprocess_input
 from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
+import keras
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
 
+
+
+# Model parameter (Resnet case)
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
 
 class Interpreter:
     """
@@ -441,6 +466,150 @@ class Interpreter:
             verbose=1,
             use_multiprocessing=True,
             workers=2
+        )
+
+        return model, model_out
+
+    def resnet_layer(self,
+                     inputs,
+                     num_filters=16,
+                     kernel_size=3,
+                     strides=1,
+                     activation='relu',
+                     batch_normalization=True,
+                     conv_first=True):
+        """2D Convolution-Batch Normalization-Activation stack builder
+
+        # Arguments
+            inputs (tensor): input tensor from input image or previous layer
+            num_filters (int): Conv2D number of filters
+            kernel_size (int): Conv2D square kernel dimensions
+            strides (int): Conv2D square stride dimensions
+            activation (string): activation name
+            batch_normalization (bool): whether to include batch normalization
+            conv_first (bool): conv-bn-activation (True) or
+                bn-activation-conv (False)
+
+        # Returns
+            x (tensor): tensor as input to the next layer
+        """
+        conv = Conv2D(num_filters,
+                      kernel_size=kernel_size,
+                      strides=strides,
+                      padding='same',
+                      kernel_initializer='he_normal',
+                      kernel_regularizer=l2(1e-4))
+
+        x = inputs
+        if conv_first:
+            x = conv(x)
+            if batch_normalization:
+                x = BatchNormalization()(x)
+            if activation is not None:
+                x = Activation(activation)(x)
+        else:
+            if batch_normalization:
+                x = BatchNormalization()(x)
+            if activation is not None:
+                x = Activation(activation)(x)
+            x = conv(x)
+        return x
+
+    def resnet_v1(
+        self,
+        input_shape,
+        depth,
+        train_images,
+        test_images,
+        validation_images,
+        num_classes=10,
+    ):
+        """ResNet Version 1 Model builder [a]
+
+        Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+        Last ReLU is after the shortcut connection.
+        At the beginning of each stage, the feature map size is halved
+        (downsampled)
+        by a convolutional layer with strides=2, while the number of filters is
+        doubled. Within each stage, the layers have the same number filters
+        and the
+        same number of filters.
+        Features maps sizes:
+        stage 0: 32x32, 16
+        stage 1: 16x16, 32
+        stage 2:  8x8,  64
+        The Number of parameters is approx the same as Table 6 of [a]:
+        ResNet20 0.27M
+        ResNet32 0.46M
+        ResNet44 0.66M
+        ResNet56 0.85M
+        ResNet110 1.7M
+
+        # Arguments
+            input_shape (tensor): shape of input image tensor
+            depth (int): number of core convolutional layers
+            num_classes (int): number of classes (CIFAR10 has 10)
+
+        # Returns
+            model (Model): Keras model instance
+        """
+        if (depth - 2) % 6 != 0:
+            raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+        # Start model definition.
+        num_filters = 16
+        num_res_blocks = int((depth - 2) / 6)
+
+        inputs = Input(shape=input_shape)
+        x = self.resnet_layer(inputs=inputs)
+        # Instantiate the stack of residual units
+        for stack in range(3):
+            for res_block in range(num_res_blocks):
+                strides = 1
+                if stack > 0 and res_block == 0:  # first layer but not first stack
+                    strides = 2  # downsample
+                y = self.resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 strides=strides)
+                y = self.resnet_layer(inputs=y,
+                                 num_filters=num_filters,
+                                 activation=None)
+                if stack > 0 and res_block == 0:  # first layer but not first stack
+                    # linear projection residual shortcut connection to match
+                    # changed dims
+                    x = self.resnet_layer(inputs=x,
+                                     num_filters=num_filters,
+                                     kernel_size=1,
+                                     strides=strides,
+                                     activation=None,
+                                     batch_normalization=False)
+                x = keras.layers.add([x, y])
+                x = Activation('relu')(x)
+            num_filters *= 2
+
+        # Add classifier on top.
+        # v1 does not use BN after last shortcut connection-ReLU
+        x = AveragePooling2D(pool_size=8)(x)
+        y = Flatten()(x)
+        outputs = Dense(num_classes,
+                        activation='softmax',
+                        kernel_initializer='he_normal')(y)
+
+        # Instantiate model.
+        model = Model(inputs=inputs, outputs=outputs)
+
+        model.compile(
+            loss='categorical_crossentropy',
+            optimizer=Adam(learning_rate=0.001),
+            metrics=['accuracy']
+        )
+        model.summary()
+
+        model_out = model.fit_generator(
+            train_images,
+            steps_per_epoch=len(train_images.classes) // self.batch_size,
+            epochs=self.epochs,
+            validation_data=validation_images,
+            validation_steps=len(validation_images.classes) // self.batch_size
         )
 
         return model, model_out
