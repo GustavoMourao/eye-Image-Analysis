4faf73cbebf1d36468bfaee6b50ac57fdb2ea49c
GustavoMourao <gustavolmourao@gmail.com>
2019-12-20 20:16:38 +0000

    fix conflicts cont

--- Interpreter.py
+++ Interpreter.py
@@ -12,7 +12,6 @@ from keras.models import Model
 # from WindowOpt.WindowsOpt import *
 from Graphs import Graphs
 # import efficientnet.keras as efn
-from keras import backend as K
 # import tensorflow as tf
 from keras.layers.normalization import BatchNormalization
 from sklearn.metrics import accuracy_score
@@ -28,7 +27,33 @@ from efficientnet_keras_transfer_learning.efficientnet import center_crop_and_re
 from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
+import keras
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
 
+
+
+# Model parameter (Resnet case)
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
 
 class Interpreter:
     """
@@ -275,20 +300,69 @@ class Interpreter:
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(32, (3, 3)))
+        model.add(Conv2D(16, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
         # Convolutional Kernel middle layer.
-        model.add(Conv2D(64, (3, 3)))
+        model.add(
+          Conv2D(
+            24,
+            (3, 3)
+            )
+        )
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(num_mid_kernel, (5, 5)))
+        model.add(Conv2D(24, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(128, (4, 4)))
+        model.add(Conv2D(40, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(40, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(80, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(80, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(80, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(112, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(112, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(192, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(192, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(192, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(192, (5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(320, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -328,6 +402,7 @@ class Interpreter:
         train_images,
         test_images,
         validation_images,
+        pretrained=True,
         topology=0
     ):
         """
@@ -339,23 +414,29 @@ class Interpreter:
             train_image: train set of data
             test_image: test set of data
             validation_images: validation set of data
+            pretrained: [True] - Pretrained weights / [False] - Train weights
             topology: (0) - EfficientNetB0; (1) - EfficientNetB1; ...
 
         Return:
         ---------
             loss and accuracy graph; model
         """
-        conv_base = self.__get_eff_model(topology)
+        conv_base = self.__get_eff_model(pretrained, topology)
 
         model = models.Sequential()
         model.add(conv_base)
+        model.summary()
+
         model.add(layers.GlobalMaxPooling2D(name="gap"))
         # model.add(layers.Flatten(name="flatten"))
         model.add(layers.Dropout(
             0.2,
             name="dropout_out"
         ))
+<<<<<<< HEAD
         print('Net before add:')
+=======
+>>>>>>> 9040d3e81bb602d0cda84cb771c62097a200a740
         model.summary()
 
         # model.add(layers.Dense(256, activation='relu', name="fc1"))
@@ -384,13 +465,158 @@ class Interpreter:
 
         model_out = model.fit_generator(
             train_images,
-            steps_per_epoch = len(train_images.classes) // self.batch_size,
+            steps_per_epoch=len(train_images.classes) // self.batch_size,
             epochs=self.epochs,
             validation_data=validation_images,
             validation_steps=len(validation_images.classes) // self.batch_size,
             verbose=1,
             use_multiprocessing=True,
             workers=2
+        )
+
+        return model, model_out
+
+    def resnet_layer(self,
+                     inputs,
+                     num_filters=16,
+                     kernel_size=3,
+                     strides=1,
+                     activation='relu',
+                     batch_normalization=True,
+                     conv_first=True):
+        """2D Convolution-Batch Normalization-Activation stack builder
+
+        # Arguments
+            inputs (tensor): input tensor from input image or previous layer
+            num_filters (int): Conv2D number of filters
+            kernel_size (int): Conv2D square kernel dimensions
+            strides (int): Conv2D square stride dimensions
+            activation (string): activation name
+            batch_normalization (bool): whether to include batch normalization
+            conv_first (bool): conv-bn-activation (True) or
+                bn-activation-conv (False)
+
+        # Returns
+            x (tensor): tensor as input to the next layer
+        """
+        conv = Conv2D(num_filters,
+                      kernel_size=kernel_size,
+                      strides=strides,
+                      padding='same',
+                      kernel_initializer='he_normal',
+                      kernel_regularizer=l2(1e-4))
+
+        x = inputs
+        if conv_first:
+            x = conv(x)
+            if batch_normalization:
+                x = BatchNormalization()(x)
+            if activation is not None:
+                x = Activation(activation)(x)
+        else:
+            if batch_normalization:
+                x = BatchNormalization()(x)
+            if activation is not None:
+                x = Activation(activation)(x)
+            x = conv(x)
+        return x
+
+    def resnet_v1(
+        self,
+        input_shape,
+        depth,
+        train_images,
+        test_images,
+        validation_images,
+        num_classes=1,
+    ):
+        """ResNet Version 1 Model builder [a]
+
+        Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+        Last ReLU is after the shortcut connection.
+        At the beginning of each stage, the feature map size is halved
+        (downsampled)
+        by a convolutional layer with strides=2, while the number of filters is
+        doubled. Within each stage, the layers have the same number filters
+        and the
+        same number of filters.
+        Features maps sizes:
+        stage 0: 32x32, 16
+        stage 1: 16x16, 32
+        stage 2:  8x8,  64
+        The Number of parameters is approx the same as Table 6 of [a]:
+        ResNet20 0.27M
+        ResNet32 0.46M
+        ResNet44 0.66M
+        ResNet56 0.85M
+        ResNet110 1.7M
+
+        # Arguments
+            input_shape (tensor): shape of input image tensor
+            depth (int): number of core convolutional layers
+            num_classes (int): number of classes (CIFAR10 has 10)
+
+        # Returns
+            model (Model): Keras model instance
+        """
+        if (depth - 2) % 6 != 0:
+            raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+        # Start model definition.
+        num_filters = 16
+        num_res_blocks = int((depth - 2) / 6)
+
+        inputs = Input(shape=input_shape)
+        x = self.resnet_layer(inputs=inputs)
+        # Instantiate the stack of residual units
+        for stack in range(3):
+            for res_block in range(num_res_blocks):
+                strides = 1
+                if stack > 0 and res_block == 0:  # first layer but not first stack
+                    strides = 2  # downsample
+                y = self.resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 strides=strides)
+                y = self.resnet_layer(inputs=y,
+                                 num_filters=num_filters,
+                                 activation=None)
+                if stack > 0 and res_block == 0:  # first layer but not first stack
+                    # linear projection residual shortcut connection to match
+                    # changed dims
+                    x = self.resnet_layer(inputs=x,
+                                     num_filters=num_filters,
+                                     kernel_size=1,
+                                     strides=strides,
+                                     activation=None,
+                                     batch_normalization=False)
+                x = keras.layers.add([x, y])
+                x = Activation('relu')(x)
+            num_filters *= 2
+
+        # Add classifier on top.
+        # v1 does not use BN after last shortcut connection-ReLU
+        x = AveragePooling2D(pool_size=8)(x)
+        y = Flatten()(x)
+        outputs = Dense(num_classes,
+                        activation='softmax',
+                        kernel_initializer='he_normal')(y)
+
+        # Instantiate model.
+        model = Model(inputs=inputs, outputs=outputs)
+
+        model.compile(
+            # loss='categorical_crossentropy',
+            loss='sparse_categorical_crossentropy',
+            optimizer=optimizers.RMSprop(lr=2e-5),
+            metrics=['acc']
+        )
+        model.summary()
+
+        model_out = model.fit_generator(
+            train_images,
+            steps_per_epoch=len(train_images.classes) // self.batch_size,
+            epochs=self.epochs,
+            validation_data=validation_images,
+            validation_steps=len(validation_images.classes) // self.batch_size
         )
 
         return model, model_out
@@ -426,8 +652,8 @@ class Interpreter:
         pred = model.predict_generator(
             test_images
         )
-        pred[pred <= 0.5] = 0
-        pred[pred > 0.5] = 1
+        pred[pred <= 0.5] = 1
+        pred[pred > 0.5] = 0
 
         graphs = Graphs()
         graphs.show_confusion_matrix(
@@ -457,7 +683,7 @@ class Interpreter:
             model.save_weights('model_simple.h5')
             print('Saved model')
 
-    def __get_eff_model(self, topology):
+    def __get_eff_model(self, pretrained, topology):
         """
         Return topology of efficientnet. Options: EfficentNetB0 - B7.
 
@@ -469,58 +695,63 @@ class Interpreter:
         ---------
             choosed model
         """
+        if pretrained:
+            load_weight = 'imagenet'
+        else:
+            load_weight = None
+
         if topology == 0:
             return Net0(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 1:
             return Net1(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 2:
             return Net2(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 3:
             return Net3(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 4:
             return Net4(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 5:
             return Net5(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 6:
             return Net6(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
 
         if topology == 7:
             return Net7(
-                weights='imagenet',
+                weights=load_weight,
                 include_top=False,
                 input_shape=self.image_shape
             )
